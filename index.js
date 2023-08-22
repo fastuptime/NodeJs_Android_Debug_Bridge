c1bd3b8afe4ec8aaf9659b1322093c91e4a53177
Can <63351166+fastuptime@users.noreply.github.com>
2023-08-22 11:15:36 +0300

    Add files via upload
    <3

---
+++ index.js
@@ -0,0 +1,18 @@
+var adb = require('adbkit')
+var client = adb.createClient()
+
+client.trackDevices() // Listen for devices on the side
+    .then(function (tracker) {
+        tracker.on('add', function (device) {
+            console.log('Device %s was plugged in', device.id)
+        })
+        tracker.on('remove', function (device) {
+            console.log('Device %s was unplugged', device.id)
+        })
+        tracker.on('end', function () {
+            console.log('Tracking stopped')
+        })
+    })
+    .catch(function (err) {
+        console.error('Something went wrong:', err.stack)
+    })
